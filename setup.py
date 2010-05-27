4b073e0c470bccd59e8094814ae76bb5efed0d87
Nick Piper <nick.piper@cgi.com>
2010-05-27 12:06:04 +0000

    Add 4 more macros which are distributed without using setuptools in their upstream. See #101
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@355 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'MiscMacrosPlugin',
+    version = '0.1',
+    author = 'Nick Piper',
+    author_email = 'nick.piper@logica.com',
+    description = 'Miscellaneous Trac macros which are not packaged with setuptools upstream.',
+    license = "Mixed",
+    url = "http://define4.trac.uk.logica.com",
+    packages = ['miscmacrosplugin'],
+    entry_points = {'trac.plugins': [
+        'miscmacrosplugin.MilestoneCompactMacro = miscmacrosplugin.MilestoneCompactMacro',
+        'miscmacrosplugin.PlannedMilestones = miscmacrosplugin.PlannedMilestones',
+        'miscmacrosplugin.TicketBox = miscmacrosplugin.TicketBox',
+        'miscmacrosplugin.WikiCalendarMacro = miscmacrosplugin.WikiCalendarMacro',
+        ]},
+    install_requires = [''],
+)
