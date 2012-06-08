df9a4a1db704a942c92443c2fccda6f01cfa1bf3
Nick Piper <nick.piper@cgi.com>
2012-06-08 12:14:15 +0000

    Merge [8134] to 4.3.x for #1993
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/releases/4.3.x@8160 484df14f-75d2-4b3f-8ba0-490e05069a01

--- miscmacrosplugin/MediahubPlayerMacro.py
+++ miscmacrosplugin/MediahubPlayerMacro.py
@@ -20,19 +20,20 @@ class MediahubPlayerMacro(WikiMacroBase):
         template_dict = {'height': int(kw.get('height', 253)),
                          'width':  int(kw.get('width', 450)),
                          'SiteId': kw['site'],
-                         'chapterId': kw['chapter']}
+                         'chapterId': kw['chapter'],
+                         'scheme': formatter.req.scheme}
         template = """
 <object id="%(SiteId)s" 
         name="MediahubVideoPlayer"  
         type="application/x-shockwave-flash" 
-        data="http://players.mymediahub.com/MediaHubPlayer.swf" height="%(height)s" width="%(width)s">
-<param name="movie" value="http://players.mymediahub.com/MediaHubPlayer.swf" />
+        data="%(scheme)s://players.mymediahub.com/MediaHubPlayer.swf" height="%(height)s" width="%(width)s">
+<param name="movie" value="%(scheme)s://players.mymediahub.com/MediaHubPlayer.swf" />
 <param name="allowFullScreen" value="true" />
 <param name="allowScriptAccess" value="always" />
 <param name="wmode" value="transparent" />
-<param name="flashvars" value="ServiceUrl=http://sites.mymediahub.com/Services/&SiteId=%(SiteId)s" />
-<a href="http://sites.mymediahub.com/Devices/Services/GetChapterVideo.ashx?chapterId=%(chapterId)s&siteId=%(SiteId)s">
-<img border="0" alt="Video" src="http://sites.mymediahub.com/Devices/Services/GetEmbedPreviewStill.ashx?siteId=%(SiteId)s" 
+<param name="flashvars" value="ServiceUrl=%(scheme)s://sites.mymediahub.com/Services/&SiteId=%(SiteId)s" />
+<a href="%(scheme)s://sites.mymediahub.com/Devices/Services/GetChapterVideo.ashx?chapterId=%(chapterId)s&siteId=%(SiteId)s">
+<img border="0" alt="Video" src="%(scheme)s://sites.mymediahub.com/Devices/Services/GetEmbedPreviewStill.ashx?siteId=%(SiteId)s" 
      width="%(width)s" height="%(height)s" 
      class="MediahubPreviewStill" />
 </a>
